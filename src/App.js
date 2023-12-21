c40844ec8b8db7a5eb55993527f3b6a58a271cb6
phillipvdh <147607190+phillipvdh@users.noreply.github.com>
2023-12-21 08:43:57 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,98 +1,98 @@
-import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { debounce } from 'throttle-debounce';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
+import { useState, useEffect } from 'react';
 
 const bookshelves = [
   { key: 'currentlyReading', name: 'Currently Reading' },
   { key: 'wantToRead', name: 'Want to Read' },
   { key: 'read', name: 'Read' }
 ];
-class BooksApp extends Component {
-  state = {
-    myBooks: [],
-    searchBooks: [],
-    error: false
-  };
-  componentDidMount = () => {
+
+function BooksApp() {
+  const [myBooks, setMyBooks] = useState([]);
+  const [searchBooks, setSearchBooks] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
     BooksAPI.getAll()
       .then(books => {
-        this.setState({ myBooks: books });
+        setMyBooks(books);
       })
       .catch(err => {
         console.log(err);
-        this.setState({ error: true });
+        setError(true);
+      });
+  }, []); 
+
+  const moveBook = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then(() => {
+        if (shelf === 'none') {
+          setMyBooks(prevBooks => prevBooks.filter(b => b.id !== book.id));
+        } else {
+          book.shelf = shelf;
+          setMyBooks(prevBooks => [...prevBooks.filter(b => b.id !== book.id), book]);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        setError(true);
       });
   };
-  moveBook = (book, shelf) => {
-    BooksAPI.update(book, shelf).catch(err => {
-      console.log(err);
-      this.setState({ error: true });
-    });
-    if (shelf === 'none') {
-      this.setState(prevState => ({
-        myBooks: prevState.myBooks.filter(b => b.id !== book.id)
-      }));
-    } else {
-      book.shelf = shelf;
-      this.setState(prevState => ({
-        myBooks: prevState.myBooks.filter(b => b.id !== book.id).concat(book)
-      }));
-    }
-  };
-  searchForBooks = debounce(300, false, query => {
+
+  const searchForBooks = debounce(300, false, query => {
     if (query.length > 0) {
       BooksAPI.search(query).then(books => {
         if (books.error) {
-          this.setState({ searchBooks: [] });
+          setSearchBooks([]);
         } else {
-          this.setState({ searchBooks: books });
+          setSearchBooks(books);
         }
       });
     } else {
-      this.setState({ searchBooks: [] });
+      setSearchBooks([]);
     }
   });
-  resetSearch = () => {
-    this.setState({ searchBooks: [] });
+
+  const resetSearch = () => {
+    setSearchBooks([]);
   };
 
-  render() {
-    const { myBooks, searchBooks, error } = this.state;
-    if (error) {
-      return <div>Network error. Please try again later.</div>;
-    }
-    return (
-      <div className="app">
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <ListBooks
-              bookshelves={bookshelves}
-              books={myBooks}
-              onMove={this.moveBook}
-            />
-          )}
-        />
-        <Route
-          path="/search"
-          render={() => (
-            <SearchBooks
-              searchBooks={searchBooks}
-              myBooks={myBooks}
-              onSearch={this.searchForBooks}
-              onMove={this.moveBook}
-              onResetSearch={this.resetSearch}
-            />
-          )}
-        />
-      </div>
-    );
+  if (error) {
+    return <div>Network error. Please try again later.</div>;
   }
+
+  return (
+    <div className="app">
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <ListBooks
+            bookshelves={bookshelves}
+            books={myBooks}
+            onMove={moveBook}
+          />
+        )}
+      />
+      <Route
+        path="/search"
+        render={() => (
+          <SearchBooks
+            searchBooks={searchBooks}
+            myBooks={myBooks}
+            onSearch={searchForBooks}
+            onMove={moveBook}
+            onResetSearch={resetSearch}
+          />
+        )}
+      />
+    </div>
+  );
 }
 
 export default BooksApp;
